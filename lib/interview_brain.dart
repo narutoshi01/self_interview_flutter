ad0f158e4de04a8a387a964944edcee09cefee21
sakamichi-jewel <taigayyy@gmail.com>
2020-04-23 13:32:06 +0900

    create InterviewBrain class, set default question and answers

---
+++ lib/interview_brain.dart
@@ -0,0 +1,12 @@
+import 'interview_question.dart';
+
+class InterViewBrain {
+  List<InterviewQuestion> _questionBox = [
+    InterviewQuestion(
+        questionText: '志望動機を教えてください', questionAnswer: '志望動機に対する返答'),
+    InterviewQuestion(
+        questionText: 'あなたの強みを教えてください', questionAnswer: '強みに対する返答'),
+    InterviewQuestion(
+        questionText: 'あなたの弱みを教えてください', questionAnswer: '弱みに対する返答'),
+  ];
+}
